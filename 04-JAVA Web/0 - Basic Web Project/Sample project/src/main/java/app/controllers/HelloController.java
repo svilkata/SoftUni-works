da394bc3c8243a86d8f1fec7f0e3be4a81bc0888
svilkata <svilkata_sh@abv.bg>
2022-04-30 00:14:30 +0300

    Adding folder JAVA Web and moving basic web project in that folder

---
+++ 04-JAVA Web/0 - Basic Web Project/Sample project/src/main/java/app/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public ModelAndView home(ModelAndView modelAndView) {
+        modelAndView.setViewName("hello.html");
+        modelAndView.addObject("username", "Pesho");
+
+        return modelAndView;
+    }
+}
